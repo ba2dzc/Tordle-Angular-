33637908c2e67b9ffab2809139bde87fdc3ccf3d
ba2dzc <batu.duzcu@gmail.com>
2022-07-20 14:59:48 +0300

    Projenin başlangıcı.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
