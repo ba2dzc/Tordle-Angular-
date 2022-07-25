344cf5323cf5d8a6780976fe9c942ab528037279
ba2dzc <batu.duzcu@gmail.com>
2022-07-25 18:18:25 +0300

    Klavyeyi ve yazı kutularını tanımladım.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
+  area = [
+    [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
+    [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
+    [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
+    [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
+    [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
+    [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }]]
 
+  klavye1 = [
+    { tus: 'E' },    { tus: 'R' },    { tus: 'T' },    { tus: 'Y' },    { tus: 'U' },
+    { tus: 'I' },    { tus: 'O' },    { tus: 'P' },    { tus: 'Ğ' },    { tus: 'Ü' }
+  ]
+  klavye2 = [
+    { tus: 'A' },    { tus: 'S' },    { tus: 'D' },    { tus: 'F' },    { tus: 'G' },
+    { tus: 'H' },    { tus: 'J' },    { tus: 'K' },    { tus: 'L' },    { tus: 'Ş' },
+    { tus: 'İ' }
+  ]
+  klavye3 = [
+    { tus: 'Enter' },    { tus: 'Z' },    { tus: 'C' },    { tus: 'V' },
+    { tus: 'B' },    { tus: 'N' },    { tus: 'M' },    { tus: 'Ö' },    { tus: 'Ç' },
+    { tus: 'Sil' }
+  ]
   ngOnInit(): void {
   }
 
