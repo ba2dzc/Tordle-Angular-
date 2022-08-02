881a0bd69fc6a86878a111e2f59cc95d221059d3
ba2dzc <batu.duzcu@gmail.com>
2022-08-02 20:41:23 +0300

    Kelime seçme ve kontrol etme fonksiyonları eklendi

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,7 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { kelime } from './words';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -8,7 +10,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
   area = [
     [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
     [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
@@ -16,10 +18,15 @@ export class MainComponent implements OnInit {
     [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
     [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }],
     [{ tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }, { tus: '' }]]
-
+  @ViewChildren('denenen') denenenSat!: QueryList<ElementRef>;
   satir = 0;
   sutun = 0;
   mesaj = "";
+  cevap = this.kelimeSeç();
+  url="https://sozluk.gov.tr/gts?ara="+this.cevap.toLocaleLowerCase();
+  public sozluk:Array<any>[]=[];
+  harfler = (Array.from(this.cevap));
+
   klavye1 = [
     { tus: 'E' }, { tus: 'R' }, { tus: 'T' }, { tus: 'Y' }, { tus: 'U' },
     { tus: 'I' }, { tus: 'O' }, { tus: 'P' }, { tus: 'Ğ' }, { tus: 'Ü' }
@@ -36,42 +43,112 @@ export class MainComponent implements OnInit {
   ]
   keyboard = [...this.klavye1, ...this.klavye2, ...this.klavye3];
 
+  kelimeSeç() {
+    var index = Math.floor(Math.random() * 5535)
+    var cevap = kelime[index];
+    return cevap
+  }
+
+
+  async bildir(sure: number) {
+    const msg = document.getElementById('hideMe');
+
+    if (msg != null) {
+      msg.textContent=this.mesaj
+      msg.style.opacity = '1';
+      await new Promise(f => setTimeout(f, sure));
+      msg.style.opacity = '0';
+    }
+  }
+
   catchbtn(tus: any) {
     if (this.sutun < 5) {
-      
+
       this.area[this.satir][this.sutun] = { tus: tus }
       console.log({ area: this.area })
       this.sutun++;
     }
   }
+
   deletebtn() {
     if (this.sutun != 0) {
       this.sutun--;
-      this.area[this.satir][this.sutun] = { tus: ' ' }
+      this.area[this.satir][this.sutun] = { tus: '' }
     }
   }
+
+
   async gonder() {
+    console.log(this.cevap)
     let veri = "";
+    let verindex = new Array;
+
     if (this.sutun > 4) {
       for (let index = 0; index < 5; index++) {
         veri = veri + this.area[this.satir][index].tus
+        verindex.push(this.area[this.satir][index].tus)
+        // console.log(verindex[index])
+        // console.log(this.harfler[index])
       }
-      console.log(veri)
-      this.satir++;
-      this.sutun = 0;
-      this.mesaj = "hobbidi"
+      if (!kelime.includes(veri)) {
+        this.mesaj = "Bu kelime geçersiz"
+        this.bildir(1000)
+        const denenensat = this.denenenSat.get(this.satir)?.nativeElement as HTMLElement;
+        denenensat.classList.add('salla');
+        setTimeout(() => {
+          denenensat.classList.remove('salla')
+        }, 500);
+      }
+      else {
+        // const harfler:{[tus:string]:number}={};
+        // for(const harf of this.cevap){
+        //   const durum=harfler[harf];
+        //   if(durum==null){
+        //     harfler[harf]=0;
+        //   } harfler[harf]++;
+        // } console.log(harfler)
+        if (veri === this.cevap) {
+          fetch(this.url)
+          .then(res=>res.json())
+          this.mesaj = "Bravo"
+          this.bildir(60000)
+        }
+
+        for (let index = 0; index < 5; index++) {
+          this.area[this.satir].map(async (item, index) => {
+            var box = document.getElementById("box_" + this.satir + index)
+            if (this.cevap.includes(item.tus)) {
+              if (item.tus == this.cevap[index]){
+                box?.classList.add("yesil")
+                await this.wait(180);}
+                else{
+              box?.classList.add("sari")
+              await this.wait(180);}
+              }
+            else{
+              box?.classList.add("gri")
+              await this.wait(180);}
+          }
+          )
+          await this.wait(180);
+        }
+        console.log(veri)
+        this.satir++;
+        this.sutun = 0;
+      }
     }
     else {
-      this.mesaj = "Bu kelime geçersiz.";
+      this.mesaj = "Yetersiz harf sayısı.";
+      this.bildir(1000)
     }
-    const msg = document.getElementById('hideMe');
-    
-    if (msg!=null) {
-      msg.style.opacity='1';
-      await new Promise(f => setTimeout(f, 1000));
-      msg.style.opacity='0';
-    }
-    
+
+  }
+  private async wait(ms: number) {
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    })
   }
 
   @HostListener('document:keydown', ['$event'])
@@ -92,8 +169,13 @@ export class MainComponent implements OnInit {
       };
     }
   }
-
   ngOnInit(): void {
+    this.http.get<Array<any>>(this.url).subscribe(data=>{
+      this.sozluk=data;
+      
+      
+    })
+    
   }
 
 }
